06697f2e513fd62046c4198ef240055f3877ecbf
Bekasyl <devbekasyl@gmail.com>
2025-01-20 10:00:48 +0500

    feat: add headers

--- parsers/kaspi_parser.py
+++ parsers/kaspi_parser.py
@@ -11,6 +11,19 @@ class KaspiParser(BaseParser):
             start_time = time.time()
             try:
                 page = await self.context.new_page()
+                await page.set_extra_http_headers({
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "DNT": "1",
+                    "Connection": "keep-alive",
+                    "Upgrade-Insecure-Requests": "1",
+                    "Sec-Fetch-Dest": "document",
+                    "Sec-Fetch-Mode": "navigate",
+                    "Sec-Fetch-Site": "none",
+                    "Sec-Fetch-User": "?1"
+                })
                 await page.goto(url, wait_until='networkidle')
                 await page.wait_for_timeout(2000)
 
--- parsers/ozon_parser.py
+++ parsers/ozon_parser.py
@@ -11,16 +11,21 @@ class OzonParser(BaseParser):
             start_time = time.time()
             try:
                 # Use proxy-enabled page
-                page = await self.create_page()
+                page = await self.context.new_page()
                 
                 # Configure page headers
                 await page.set_extra_http_headers({
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                    'Accept-Language': 'en-US,en;q=0.5',
-                    'Accept-Encoding': 'gzip, deflate, br',
-                    'DNT': '1',
-                    'Connection': 'keep-alive',
-                    'Upgrade-Insecure-Requests': '1'
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                    "Accept-Language": "ru-KZ,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "DNT": "1",
+                    "Connection": "keep-alive",
+                    "Upgrade-Insecure-Requests": "1",
+                    "Sec-Fetch-Dest": "document",
+                    "Sec-Fetch-Mode": "navigate",
+                    "Sec-Fetch-Site": "none",
+                    "Sec-Fetch-User": "?1"
                 })
 
                 await page.goto(url, wait_until='networkidle')
